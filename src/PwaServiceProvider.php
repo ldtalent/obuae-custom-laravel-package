8591edcf42fc8f115ff74e7d67db5a028c59936a
Emmanuel Obua <eobua6882@gmail.com>
2022-08-19 17:43:00 +0300

    Created a service provider to register a custom command to publish pwa assets

---
+++ src/PwaServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ldtalent\Pwa;
+
+use Illuminate\Support\ServiceProvider;
+
+use Ldtalent\Pwa\commands\PublishPwaAssets;
+
+class PwaServiceProvider extends ServiceProvider
+{
+	
+	/**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+	public function boot()
+	{
+
+	}
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    	$this->app->singleton('pwa-laravel:publish', function ($app) {
+    		return new PublishPwaAssets();
+    	});
+
+    	$this->commands([
+    		'pwa-laravel:publish',
+    	]);
+    }
+
+}
