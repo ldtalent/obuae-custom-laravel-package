b9fb29d70d63a7a346ceabb0b405bd9b7a59127f
Emmanuel Obua <eobua6882@gmail.com>
2022-08-26 13:26:57 +0300

    added a full documentation for some components of the package

--- src/Commands/PublishPwaAssets.php
+++ src/Commands/PublishPwaAssets.php
@@ -13,7 +13,7 @@ class PublishPwaAssets extends Command
      *
      * @var string
      */
-    protected $name = 'pwa-laravel:publish';
+    protected $signature = 'pwa-laravel:publish';
 
     /**
      * The console command description.
@@ -31,40 +31,72 @@ class PublishPwaAssets extends Command
     {
         parent::__construct();
 
+        /**Get the composer instance from the application container.
+         * Set it to the composer public property of this class
+         */
         $this->composer = app()['composer'];
     }
 	
     public function handle()
     {
+
+        /** Get the public directory of a laravel application*/
         $publicDir = public_path();
         
+        /** Get the manifest template from manifest.stub file*/
         $manifestTemplate = file_get_contents(__DIR__.'/../Stubs/manifest.stub');
+
+        /** Create a manifest.json file in the application's public directory*/
         $this->createFile($publicDir. DIRECTORY_SEPARATOR, 'manifest.json', $manifestTemplate);
+
+        /** Notify the user with the results of the operation.*/
         $this->info('manifest.json file is published.');
         
+        /** Get the offline template from offline.stub file*/
         $offlineHtmlTemplate = file_get_contents(__DIR__.'/../Stubs/offline.stub');
+
+        /** Create a offline.html file in the application's public directory*/
         $this->createFile($publicDir. DIRECTORY_SEPARATOR, 'offline.html', $offlineHtmlTemplate);
+
+        /** Notify the user with the results of the operation.*/
         $this->info('offline.html file is published.');     
         
+        /** Get the sw template from sw.stub file*/
         $swTemplate = file_get_contents(__DIR__.'/../Stubs/sw.stub');
+
+        /** Create a sw.js file in the application's public directory*/
         $this->createFile($publicDir. DIRECTORY_SEPARATOR, 'sw.js', $swTemplate);
+
+        /** Notify the user with the results of the operation.*/
         $this->info('sw.js (Service Worker) file is published.');     
 
+        /**Run the composer method below to regenerate the list of all classes 
+         * that need to be included in the project
+         * */
         $this->info('Generating autoload files');
         $this->composer->dumpOptimized();
 
         $this->info('Greeting!.. Enjoy your pwa laravel app...');
+
     }
 
+    
     public static function createFile($path, $fileName, $contents)
     {
+
+        /**Check if a file exists in the public directory,
+         * If not create that file and give is a public access permission.
+         * Else, get the file existing and replace the contents with the new one.
+         */
         if (!file_exists($path)) {
             mkdir($path, 0755, true);
         }
 
+
         $path = $path.$fileName;
 
         file_put_contents($path, $contents);
+        
     }
 
 }
--- src/PwaServiceProvider.php
+++ src/PwaServiceProvider.php
@@ -26,10 +26,17 @@ class PwaServiceProvider extends ServiceProvider
      */
     public function register()
     {
+
+        /** Here we are restricting the instantiation of a class to a single object. 
+         * This is useful when only one object is required across the entire system
+        */
     	$this->app->singleton('pwa-laravel:publish', function ($app) {
     		return new PublishPwaAssets();
     	});
 
+        /** Add the list of commands registered into the commands array for 
+         * access in the artisan console.
+         * */
     	$this->commands([
     		'pwa-laravel:publish',
     	]);
